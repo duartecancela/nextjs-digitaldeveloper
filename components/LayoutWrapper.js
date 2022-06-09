8e3490172176509a815379ac528b04e55741aa85
duartecancela <duartecancela@gmail.com>
2022-06-09 14:34:41 +0100

    implement light and dark logo

--- components/LayoutWrapper.js
+++ components/LayoutWrapper.js
@@ -1,6 +1,8 @@
 import siteMetadata from '@/data/siteMetadata'
 import headerNavLinks from '@/data/headerNavLinks'
-import Logo from '@/data/logo.svg'
+import { useTheme } from 'next-themes'
+import DarkLogo from '@/data/dark-logo.svg'
+import LightLogo from '@/data/light-logo.svg'
 import Link from './Link'
 import SectionContainer from './SectionContainer'
 import Footer from './Footer'
@@ -8,6 +10,8 @@ import MobileNav from './MobileNav'
 import ThemeSwitch from './ThemeSwitch'
 
 const LayoutWrapper = ({ children }) => {
+  const { theme } = useTheme()
+
   return (
     <SectionContainer>
       <div className="flex h-screen flex-col justify-between">
@@ -15,9 +19,7 @@ const LayoutWrapper = ({ children }) => {
           <div>
             <Link href="/" aria-label={siteMetadata.headerTitle}>
               <div className="flex items-center justify-between">
-                <div className="mr-3">
-                  <Logo />
-                </div>
+                <div className="mr-3">{theme === 'light' ? <DarkLogo /> : <LightLogo />}</div>
                 {typeof siteMetadata.headerTitle === 'string' ? (
                   <div className="hidden h-6 text-2xl font-semibold sm:block">
                     {siteMetadata.headerTitle}
