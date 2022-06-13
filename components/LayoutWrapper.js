2f9078902aa3c27c33e416294cc21a74e45fe473
duartecancela <duartecancela@gmail.com>
2022-06-13 14:25:35 +0100

    update logo theme change

--- components/LayoutWrapper.js
+++ components/LayoutWrapper.js
@@ -19,7 +19,7 @@ const LayoutWrapper = ({ children }) => {
           <div>
             <Link href="/" aria-label={siteMetadata.headerTitle}>
               <div className="flex items-center justify-between">
-                <div className="mr-3">{theme === 'light' ? <DarkLogo /> : <LightLogo />}</div>
+                <div className="mr-3">{theme === 'dark' ? <LightLogo /> : <DarkLogo />}</div>
                 {typeof siteMetadata.headerTitle === 'string' ? (
                   <div className="hidden h-6 text-2xl font-semibold sm:block">
                     {siteMetadata.headerTitle}
