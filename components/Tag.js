07254c4ce7cdd6a0c16c6f63cc02269d8925b5f6
Duarte Cancela <duartecancela@gmail.com>
2022-06-17 22:42:39 +0100

    update dummy image on all posts and add tag symbol

--- components/Tag.js
+++ components/Tag.js
@@ -4,7 +4,8 @@ import kebabCase from '@/lib/utils/kebabCase'
 const Tag = ({ text }) => {
   return (
     <Link href={`/tags/${kebabCase(text)}`}>
-      <a className="mr-3 text-sm font-medium uppercase text-primary-500 hover:text-primary-600 dark:hover:text-primary-400">
+      <a className="mr-3 flex text-sm font-medium uppercase text-primary-500 hover:text-primary-600 dark:hover:text-primary-400">
+        <span># </span>
         {text.split(' ').join('-')}
       </a>
     </Link>
